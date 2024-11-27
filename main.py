835782877959b53ec84341178cb98e47ada37495
Nonyerem23 <mc.chinonye@stu.unizik.edu.ng>
2024-11-27 20:36:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Fake Fan Finder")
+print("---------------")
+anime = input("What's your favourite Anime? ")
+if anime == "One piece":
+  print("Oh really?!")
+  print()
+  character = input("Name me any of the characters? ")
+  if character == "Nami":
+    print("You got that by pure chance.")
+  else:
+    print("See! Fake One piece fan!")
+    print()
+    job = input("Okay then, what is her job on the ship? ")
+    if job == "Navigator":
+      print("Hmph!")
+      
+    else:
+      print("Chai, you don't know anything about One piece")
+      print()
+      bounty = input("What was her first bounty then? ")
+      if bounty == "Ummm...":
+        print("You must be a fake fan!")
+      elif bounty == "16,000,000 berry":
+        print("You are a true fan!")
+      else:
+        print("See! Fake One piece fan!")
+   
+        
+        
